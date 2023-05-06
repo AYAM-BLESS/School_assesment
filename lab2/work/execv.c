ef43d9264a75f11485fbdfc19644536b004b111e
AYAM-BLESS <blessbissong@gmail.com>
2023-05-06 20:57:00 +0100

    minor changes

--- lab2/work/execv.c
+++ lab2/work/execv.c
@@ -4,6 +4,5 @@ int main() {
        //A null terminated array of character pointers
        char *args[]={"./hello",NULL};
        execv(args[0],args);
-       printf("Hello World\n");
    return 0;
 }
