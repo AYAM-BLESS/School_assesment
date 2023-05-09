75d04f530b50839f4c56756065d956445d86093b
AYAM-BLESS <blessbissong@gmail.com>
2023-05-09 01:39:04 +0100

    minor changes in the code to print the pid of the parent process and that of the child

--- lab3/task1.c
+++ lab3/task1.c
@@ -1,9 +1,22 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<unistd.h>
-int main(){
-    fork();
-    printf("Welcome to CEF 4a58 \n");
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    pid_t pid;
+
+    pid = fork();
+
+    if (pid < 0) {
+        fprintf(stderr, "Fork failed");
+        return 1;
+    } else if (pid == 0) {
+        // Child process
+        printf("I am the child process (pid=%d)\n", getpid());
+    } else {
+        // Parent process
+        printf("I am the parent process (pid=%d)\n", getpid());
+    }
+
     return 0;
 }
