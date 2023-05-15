c1df1a1f3dbe827d518d190add4939dc5f68a01b
AYAM-BLESS <blessbissong@gmail.com>
2023-05-15 20:32:52 +0100

    Creating a program call main that ask the user to input an integer value and the store this value in a variable say y and then pass this value to another program. That is we are implimenting a named pipe

---
+++ lab4/task2/main1.c
@@ -0,0 +1,30 @@
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+   int fd;
+   int y;
+
+   // Create the named pipe
+   mkfifo("myfifo", 0666);
+
+   // Prompt the user to enter an integer
+   printf("In main1 process: Enter an integer number: ");
+   scanf("%d", &y);
+   printf("In main1 process: Received y = %d from the user\n", y);
+
+   // Open the named pipe for writing
+   fd = open("myfifo", O_WRONLY);
+   
+   // Write the value of y to the named pipe
+   write(fd, &y, sizeof(y));
+   
+   // Close the named pipe
+   close(fd);
+
+   return 0;
+}
