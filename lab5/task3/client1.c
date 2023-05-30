84aa39621423f2f8686cd1f96e81ca3232bad286
AYAM-BLESS <blessbissong@gmail.com>
2023-05-30 01:29:01 +0100

    This is the source code of client1. It prompts the user to enter a letter and then sends the result to the server

---
+++ lab5/task3/client1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char letter[2] = {0};
+    char buffer[1024] = {0};
+
+    // Get letter from user
+    printf("Enter a letter: ");
+    scanf("%c", letter);
+
+    // Create socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    // Set server address
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+
+    // Send letter to server
+    send(sock, letter, strlen(letter), 0);
+    printf("Sent letter to server: %c\n", letter[0]);
+
+    // Wait for response from server
+    valread = read(sock, buffer, 1024);
+
+    // Close socket
+    close(sock);
+
+    return 0;
+}
