8ba772c67e95c7c44e3805cbb25cc6402ddf53e1
AYAM-BLESS <blessbissong@gmail.com>
2023-05-30 01:53:28 +0100

    This is the code for client1, it prompts the user to enter a floating point number and then sends it to the client

---
+++ lab5/task5/client1.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    float message;
+
+    // Get integer message from user
+    printf("Enter a float message: ");
+    scanf("%f", &message);
+
+   // Create socket
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    // Set server address
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // Send float to server
+    send(sock, &message, sizeof(float), 0);
+    printf("Sent message to server: %f\n", message);
+
+    // Wait for response from server
+    valread = read(sock, &message, sizeof(message));
+
+    return 0;
+}
