950dae02c7a39b2abe853e8fcde3245c7d2e3592
AYAM-BLESS <blessbissong@gmail.com>
2023-05-30 01:38:52 +0100

    This is the code of client1. which prompts the user to enter an integer number and then sents it to the server

---
+++ lab5/task4/client1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define PORT 8080
+
+int main(int argc, char const *argv[]) {
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    int message;
+
+    // Get integer message from user
+    printf("Enter an integer message: ");
+    scanf("%d", &message);
+
+    // Create socket file descriptor
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    // Set server address
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    // Connect to server
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    // Send message to server
+    send(sock, &message, sizeof(message), 0);
+    printf("Sent message to server: %d\n", message);
+
+    // Wait for response from server
+    valread = read(sock, &message, sizeof(message));
+
+    // Close socket
+    close(sock);
+
+    return 0;
+}
