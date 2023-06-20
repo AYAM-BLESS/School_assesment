b2a3ec546814fe132c3c8b622cf8499f0cbe21c2
AYAM-BLESS <blessbissong@gmail.com>
2023-06-20 21:57:05 +0100

    Task d

---
+++ work assignment/taskd.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    float i, j, sum;
+    printf("Enter the first decimal value\n");
+    scanf("%f",&i);
+    printf("Enter the second decimal value\n");
+    scanf("%f",&j);
+    sum = i + j;
+    printf("The sum of the two decimal values is sum = %f \n",sum);
+}
